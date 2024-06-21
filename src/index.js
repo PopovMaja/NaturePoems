babdad90650a9e7fb2769a788cdcb129dfa28643
PopovMaja <167343423+PopovMaja@users.noreply.github.com>
2024-06-21 15:46:39 -0400

    added form handling and typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "Ever, ever",
+    autoStart: true,
+    cursor: "",
+    delay: 1,
+  });
+}
+
+let enterSubmitElement = document.querySelector("#enter-submit-box");
+enterSubmitElement.addEventListener("submit", generatePoem);
