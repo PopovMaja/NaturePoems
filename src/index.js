5d49e9c1afb9e7a9c58efb4a8a263a481e2e6109
PopovMaja <167343423+PopovMaja@users.noreply.github.com>
2024-06-22 13:12:22 -0400

    Improved the User Experience

--- src/index.js
+++ src/index.js
@@ -14,10 +14,13 @@ function generatePoem(event) {
   let apiKey = "594t1344a33ebb79a8c028f85db36co0";
   let prompt = `Generate nature poem about ${instructionsInput.value}`;
   let context =
-    "Please generate nature poems about provided word.Poem should be in english language and not longer then 10 lines. Please separate each line with <br/> and please sign author of the poem inside <strong> element ,IF there is actual name.If there is no name of the author,write -unknown,";
+    "Please generate nature poems about provided word.Poem should be in English language and not longer then 10 lines. Please separate each line with <br/> and please sign author of the poem inside <strong> element ,IF there is actual name.If there is no name of the author,write -unknown,";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("generating poem...");
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="loading">⏳ Generating poem about ${instructionsInput.value} </div>`;
+
   axios.get(apiURL).then(displayPoem);
 }
 
